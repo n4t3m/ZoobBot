69fbb47977dd5d389c799f0048f484483393e95a
Nathan <Nathan>
2019-06-25 13:50:01 -0700

    First Commit

---
+++ CommandHandler.cs
@@ -0,0 +1,75 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+public class CommandHandler
+{
+    private CommandService _cmds;
+    private DiscordSocketClient _client;
+    public async Task Install(DiscordSocketClient c)
+    {
+        _client = c;
+        _cmds = new CommandService();
+
+        await _cmds.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+        _client.MessageReceived += HandleCommand;
+
+
+        _client.UserJoined += AnnounceUserJoined;
+        _client.UserLeft += AnnounceUserLeft;
+    }
+
+    public async Task AnnounceUserJoined(SocketGuildUser user)
+    {
+
+    }
+
+
+    public async Task AnnounceUserLeft(SocketGuildUser user)
+    {
+        await Task.Delay(0);
+    }
+    public void code()
+    {
+
+    }
+    public async Task HandleCommand(SocketMessage s)
+    {
+
+
+        var msg = s as SocketUserMessage;
+        if (msg == null) return;
+
+        var context = new CommandContext(_client, msg);
+
+        int argPos = 0;
+        string prefix = "!";
+        if (msg.HasStringPrefix(prefix, ref argPos) || msg.HasMentionPrefix(_client.CurrentUser, ref argPos))
+        {
+
+
+            var result = await _cmds.ExecuteAsync(context, argPos, null);
+
+            if (!result.IsSuccess)
+            {
+                switch (result.ToString())
+                {
+                    default:
+
+                        await s.Channel.SendMessageAsync($"An error occurred! Details: ```" + result.ToString() + "```");
+                        break;
+                    case "UnknownCommand: Unknown command.":
+
+                        await msg.DeleteAsync();
+
+                        await s.Channel.SendMessageAsync($"Command not found! Use the command {prefix}help for a list of commands.");
+                        break;
+
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+
+namespace ShitpostBot
+{
+    class Program
+    {
+        public DiscordSocketClient Client;
+        public CommandHandler Handler;
+        static void Main(string[] args) => new Program().Start().GetAwaiter().GetResult();
+
+        public async Task Start()
+        {
+            Client = new DiscordSocketClient();
+
+            Console.WriteLine("Patrick is cute");
+
+            Handler = new CommandHandler();
+
+            await Client.LoginAsync(Discord.TokenType.Bot, "NTkyOTUyOTMzMzI3NDM3ODI4.XRJ3Qg.lNy8wZNwkpixxqUloOKNB6yIR0Q", true);
+
+            await Client.StartAsync();
+
+            await Handler.Install(Client);
+
+            Client.MessageReceived += MessageReceived;
+            Client.Log += Log;
+
+            Client.Ready += Client_Ready;
+            await Task.Delay(-1);
+
+        }
+
+        private async Task Client_Ready()
+        {
+            Console.WriteLine("SHITPOST BOT REPORTING FOR DUTY!");
+            return;
+
+        }
+
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+            Console.WriteLine("Method Called");
+            if (message.Content == "hi")
+            {
+                Console.WriteLine("Evaluate to True");
+                await message.Channel.SendMessageAsync("Hello!");
+            }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+
+
+    }
+}
+
+
