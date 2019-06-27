d4e724a51b6a9b5e6b322693067ca2cee2bbf516
Nathan <Nathan>
2019-06-27 10:45:38 -0700

    Removed coochie copypasta

--- Modules/Memes/Copypasta.cs
+++ Modules/Memes/Copypasta.cs
@@ -17,7 +17,6 @@ namespace ShitpostBot
         [Alias("copypasta list")]
         public Task CopypastaList() => ReplyAsync($"**Use all Lowercase Letters**\n" +
             $"List:\n" +
-            $"Coochie (Trans Pussy)\n" +
             $"Navy Seal\n" +
             $"Airpods\n" +
             $"Bathroom Scale\n" +
@@ -25,9 +24,6 @@ namespace ShitpostBot
             $"Hit Or Miss\n" +
             $"Family Fued\n" +
             $"Adam\n");
-
-        [Command("copypasta coochie")]
-        public Task Coochie() => ReplyAsync($"I'm trying😢 to get me some trans🤖 pussy 😺💦. Like shit💩 it ain't natural🌱but that coochie😛💦 be custom👌built🤖. Probably got some 🤟velvet 😍heated🔥interiors. Auto 😤lavender-scented🌸 lube dispensers💦 so that coochie😺 staying wet🌊 but not smelling🤢 like a zoo. Little robo🤖 hands🙌 that reach👋out and cradle🤲😍 your balls🏀⚾ while you hamming🔨 on that pseudo-snatch. A drain💧 vacuum for all👍the nuts🍆💦 you blast💥in there so you ain't🌀🌪️stirring your own porridge🥣 for round 2🤢. You guys🙎‍♂️be hating😪on trans🌈 people, even😮during their own🌗month, but there's all🌟kinds of potential there. Fuck💢all that normal🤢sloppy coochie, get yourself some trans🤖super💥 coochie. They're called🌈 trans because that😺 pussy be transforming👾your life.\n");
 
         [Command("copypasta navy seal")]
         public Task NavySeal() => ReplyAsync($"What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.");
