65a83e2fc99079b74f52be2fe501b2d73fafeb0d
Nathan <Nathan>
2019-06-28 15:30:54 -0700

    readded helpers

---
+++ Helpers/UrbanDictionary.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ShitpostBot.Helpers
+{
+    public class UrbanDictionary
+    {
+        public static async Task<KeyValuePair<bool, Data>> GetDataAsync(string query)
+        {
+
+        }
+    }
+
+    public class List
+    {
+
+    }
+
+    public class Data
+    {
+
+    }
+}
