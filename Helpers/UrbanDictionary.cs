adb1a933e7afa2c24369494f84d4247e8f643459
Nathan <Nathan>
2019-06-28 17:05:47 -0700

    fixing bot sgtart error

--- Helpers/UrbanDictionary.cs
+++ Helpers/UrbanDictionary.cs
@@ -11,17 +11,58 @@ namespace ShitpostBot.Helpers
     {
         public static async Task<KeyValuePair<bool, Data>> GetDataAsync(string query)
         {
+            using (var http = new HttpClient())
+            {
+                var result = await http.GetStringAsync($"http://api.urbandictionary.com/v0/define?term={WebUtility.UrlEncode(query)}");
+                var data = JsonConvert.DeserializeObject<Data>(result);
 
+                return new KeyValuePair<bool, Data>(data.ResultType != "no_results", data);
+            }
         }
     }
 
     public class List
     {
+        [JsonProperty("defid")]
+        public int DefId { get; set; }
 
+        [JsonProperty("word")]
+        public string Word { get; set; }
+
+        [JsonProperty("author")]
+        public string Author { get; set; }
+
+        [JsonProperty("permalink")]
+        public string Permalink { get; set; }
+
+        [JsonProperty("definition")]
+        public string Definition { get; set; }
+
+        [JsonProperty("example")]
+        public string Example { get; set; }
+
+        [JsonProperty("thumbs_up")]
+        public int ThumbsUp { get; set; }
+
+        [JsonProperty("thumbs_down")]
+        public int ThumbsDown { get; set; }
+
+        [JsonProperty("current_vote")]
+        public string CurrentVote { get; set; }
     }
 
     public class Data
     {
+        [JsonProperty("tags")]
+        public string[] Tags { get; set; }
 
+        [JsonProperty("result_type")]
+        public string ResultType { get; set; }
+
+        [JsonProperty("list")]
+        public List[] List { get; set; }
+
+        [JsonProperty("sounds")]
+        public string[] Sounds { get; set; }
     }
 }
